2aedf604071479e56c8bf9942ffccf310befe688
Marco Paulo Martins Sousa <MarcoPaulo.Martins@aqualogybs.net>
2017-09-13 11:48:44 +0200

    change port

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # Simple image that returns the container ID on http get
 
 This is a minimalist flask app that returns the container ID via HTTP GET.
+
+Run the Docker container using the command shown below.
+
+```bash
+$ docker run -d -p 5000:5000 marcomsousa/hostname
+```
--- hello.py
+++ hello.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    s = 'This is an example:' + socket.gethostname() 
+    s = 'This is an example:' + socket.gethostname()
     return s
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
